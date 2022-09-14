965cf675de642f726f3008847357126f4299fa4b
ilbrigz <ilogirb23@yahoo.com>
2022-09-14 19:03:28 +0800

    first commmit

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig .json',
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
