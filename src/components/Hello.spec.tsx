62bc6b99864525956af95e6a778d25f3c4686fdf
ilbrigz <ilogirb23@yahoo.com>
2022-09-15 05:51:08 +0800

    .

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 import { Hello } from './Hello';
 
 it('renders "Hello World"', () => {
+
+
+
+
   render(<Hello />);
+
   const element = screen.getByText(/Hello World/);
   expect(element).toBeInTheDocument();
 });
